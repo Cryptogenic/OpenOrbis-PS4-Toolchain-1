744b6c88c3144201fa2bc2560bc151a023801a11
Nikita Krapivin <alienoom@yandex.ru>
2021-12-01 21:33:10 +0500

    Initial exception support

---
+++ samples/exceptions/exceptions/main.cpp
@@ -0,0 +1,35 @@
+
+#include "../../_common/log.h"
+#include <stdexcept>
+
+void do_throw_exception() {
+	throw std::runtime_error("Catch me if you can. :p");
+}
+
+void do_throw_exception_2() {
+	throw std::runtime_error("Another one.");
+}
+
+void testcase() {
+	try {
+		do_throw_exception();
+	} catch (const std::exception& exc) {
+		DEBUGLOG << "Caught! The exception says: " << exc.what();
+	}
+}
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    DEBUGLOG << "Before testcase...";
+    try {
+    	testcase();
+	} catch (const std::exception& exc) {
+		DEBUGLOG << ".what() = " << exc.what();
+	}
+    DEBUGLOG << "Testcase PASS";
+
+
+    DEBUGLOG << "Infinite looping...";
+    for (;;);
+}
