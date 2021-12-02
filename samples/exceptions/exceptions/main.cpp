0458eb9f339419be01b88d5ce641e305c66a3e3d
Nikita Krapivin <alienoom@yandex.ru>
2021-12-02 11:19:39 +0500

    Update main.cpp

--- samples/exceptions/exceptions/main.cpp
+++ samples/exceptions/exceptions/main.cpp
@@ -2,6 +2,8 @@
 #include "../../_common/log.h"
 #include <stdexcept>
 
+std::stringstream debugLogStream;
+
 void do_throw_exception() {
 	throw std::runtime_error("Catch me if you can. :p");
 }
@@ -24,11 +26,11 @@ int main() {
     DEBUGLOG << "Before testcase...";
     try {
     	testcase();
+		do_throw_exception_2();
 	} catch (const std::exception& exc) {
 		DEBUGLOG << ".what() = " << exc.what();
+		DEBUGLOG << "Testcase PASS";
 	}
-    DEBUGLOG << "Testcase PASS";
-
 
     DEBUGLOG << "Infinite looping...";
     for (;;);
